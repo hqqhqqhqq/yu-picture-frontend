8e329cd4317640d76ec99477613cf3aa55cecb12
hqqhqqhqq <764238492@qq.com>
2025-10-18 16:15:46 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <BasicLayout />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import BasicLayout from '@/layouts/BasicLayout.vue'
+import { useLoginUserStore } from '@/stores/useLoginStore.ts'
+
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+
+</script>
+
+<style scoped></style>
---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a target="_blank"> 智能协同云图库 </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import GlobalHeader from '@/components/GlobalHeader.vue'
+</script>
+<style scoped>
+#basicLayout .header {
+  background: white;
+  color: unset;
+  margin-bottom: 16px;
+  padding-inline: 20px;
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+
+#basicLayout .content {
+  padding: 20px;
+  background: linear-gradient(to right, #fefefe, #fff);
+  margin-bottom: 28px;
+}
+</style>
