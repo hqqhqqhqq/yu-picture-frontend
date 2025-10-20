6c93a9e4a3d25f849399d9a7f36e832ff9839dd4
hqqhqqhqq <764238492@qq.com>
2025-10-20 23:07:52 +0800

    完成图片列表页

--- src/pages/AddPicturePage.vue
+++ src/pages/AddPicturePage.vue
@@ -123,7 +123,7 @@ const getTagCategoryOptions = async () => {
       }
     })
   } else {
-    message.error('加载选项失败，' + res.data.message)
+    message.error('获取标签分类列表失败，' + res.data.message)
   }
 }
 
