ab47739b7d351edddf333bcb43ea2a8428fbfd0b
hqqhqqhqq <764238492@qq.com>
2025-11-06 22:54:42 +0800

    增加侧边栏

---
+++ src/components/GlobalSider.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="globalSider">
+    <a-layout-sider v-if="loginUserStore.loginUser.id" width="200">
+      <a-menu
+        v-model:selectedKeys="current"
+        :items="menuItems"
+        mode="inline"
+        @click="doMenuClick"
+      />
+    </a-layout-sider>
+  </div>
+</template>
+<script lang="ts" setup>
+import { h, ref } from 'vue'
+import { PictureOutlined, UserOutlined } from '@ant-design/icons-vue'
+import { message } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
+import { useLoginUserStore } from '../stores/useLoginUserStore.ts'
+import { userLogoutUsingPost } from '@/api/userController.ts'
+
+const loginUserStore = useLoginUserStore()
+
+// 菜单项
+const menuItems = [
+  {
+    key: '/',
+    icon: () => h(PictureOutlined),
+    label: '公共图库',
+  },
+  {
+    key: '/admin/my_space',
+    label: '我的空间',
+    icon: () => h(UserOutlined),
+  },
+]
+
+// 路由跳转事件
+const doMenuClick = ({ key }: { key: string }) => {
+  router.push({
+    path: key,
+  })
+}
+
+const router = useRouter()
+
+// 当前要高亮的菜单项
+const current = ref<string[]>([])
+
+// 监听路由变化，更新高亮菜单项
+router.afterEach((to, from, next) => {
+  current.value = [to.path]
+})
+</script>
+
+<style scoped>
+#globalSider .ant-layout-sider {
+  background: none;
+}
+</style>
--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -4,9 +4,12 @@
       <a-layout-header class="header">
         <GlobalHeader />
       </a-layout-header>
-      <a-layout-content class="content">
-        <router-view />
-      </a-layout-content>
+      <a-layout>
+        <GlobalSider class="sider" />
+        <a-layout-content class="content">
+          <router-view />
+        </a-layout-content>
+      </a-layout>
       <a-layout-footer class="footer">
         <a target="_blank"> 智能协同云图库 </a>
       </a-layout-footer>
@@ -16,13 +19,25 @@
 
 <script lang="ts" setup>
 import GlobalHeader from '@/components/GlobalHeader.vue'
+import GlobalSider from '@/components/GlobalSider.vue'
 </script>
 <style scoped>
 #basicLayout .header {
   background: white;
   color: unset;
-  margin-bottom: 5px;
+  margin-bottom: 1px;
   padding-inline: 20px;
+}
+
+#basicLayout .sider {
+  background: #fff;
+  border-right: 0.5px solid #eee;
+  padding-top: 20px;
+}
+
+#basicLayout :deep(.ant-meni-root) {
+  border-bottom: none !important;
+  border-inline-end: none !important;
 }
 
 #basicLayout .footer {
@@ -36,7 +51,7 @@ import GlobalHeader from '@/components/GlobalHeader.vue'
 }
 
 #basicLayout .content {
-  padding: 20px;
+  padding: 28px;
   background: linear-gradient(to right, #fefefe, #fff);
   margin-bottom: 28px;
 }
